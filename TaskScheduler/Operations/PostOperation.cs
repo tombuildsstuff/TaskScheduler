d854cf87bf6d71e9f614d98110fc54c40f0174a3
orlando <operri@opentable.com>
2013-12-30 17:43:17 +0000

    mongo repository

--- TaskScheduler/Operations/PostOperation.cs
+++ TaskScheduler/Operations/PostOperation.cs
@@ -1,10 +1,22 @@
-﻿namespace TaskScheduler.Operations
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace TaskScheduler.Operations
 {
     public class PostOperation : IOperation
     {
+        internal class PostParameters
+        {
+            public string Url { get; set; }
+            public int Timeout { get; set; }
+        }
+
         public void Execute(string parameters)
         {
-            
+            var deserializedParameters = JsonConvert.DeserializeObject<PostParameters>(parameters);
+            var client = WebRequest.Create(deserializedParameters.Url);
+            client.Timeout = deserializedParameters.Timeout;
+            client.GetResponse();
         }
     }
 }
