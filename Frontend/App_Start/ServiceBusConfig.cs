ae646ae1484bd537c268d4dc724ca2382c27495c
orlando <operri@opentable.com>
2014-01-20 10:56:31 +0000

    Redis configuration

--- Frontend/App_Start/ServiceBusConfig.cs
+++ Frontend/App_Start/ServiceBusConfig.cs
@@ -28,13 +28,16 @@ namespace Frontend
             var eventStoreUserName = ConfigurationManager.AppSettings["EventStoreUserName"];
             var eventStorePassword = ConfigurationManager.AppSettings["EventStorePassword"];
             var useEventStore = bool.Parse(ConfigurationManager.AppSettings["UseEventStore"]);
+            var redisIp = ConfigurationManager.AppSettings["RedisIp"];
+            var redisPort = int.Parse(ConfigurationManager.AppSettings["RedisPort"]);
+            var redisMaxRetries = int.Parse(ConfigurationManager.AppSettings["RedisMaxRetries"]);
             eventHandler.RegisterInstance(() => new UpdateTaskResponseStatusEventHandler(mongoTaskRepository));
             eventHandler.RegisterInstance(() => new RunTaskEventHandler(new StandardDateTimeProvider(), mongoTaskRepository, new TimeSpanEvaluator(),
                 new OperationResolver()));
             eventHandler.RegisterInstance(() => new ElapsedTimeEventHandler(mongoTaskRepository, new StandardDateTimeProvider()));
             eventHandler.RegisterInstance(() => new InitializeTaskEventHandler(new TimeSpanEvaluator(), new StandardDateTimeProvider(), mongoTaskRepository));
             eventHandler.RegisterInstance(() => new InitializeTaskManagerEventHandler(new JSonConfigurationRepository(config), mongoTaskRepository));
-            eventHandler.RegisterInstance(() => new ExceptionRaisedEventHandler(new RedisLogger<LogstashLog>(new RedisConnectionFactory(new RedisConnectionWrapper(), "10.10.20.68", 6379, 3))));
+            eventHandler.RegisterInstance(() => new ExceptionRaisedEventHandler(new RedisLogger<LogstashLog>(new RedisConnectionFactory(new RedisConnectionWrapper(), redisIp, redisPort, redisMaxRetries))));
             Bus.InitializeBus(eventHandler, useEventStore 
                 ? new EventStoreRepository(new EventStoreConfiguration(eventStoreIp,eventStorePort,eventStoreUserName,eventStorePassword))
                 : null);
