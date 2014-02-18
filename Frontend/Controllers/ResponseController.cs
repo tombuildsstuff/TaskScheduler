09f746c4771bd6268065cd5f6273ad2458063821
orlando <operri@opentable.com>
2014-02-18 14:33:01 +0000

    added new controller for lbstatus

--- Frontend/Controllers/ResponseController.cs
+++ Frontend/Controllers/ResponseController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using MongoDataAccess;
 using TaskScheduler.Services;
@@ -17,6 +20,34 @@ namespace Frontend.Controllers
         public void Post([FromUri] string taskName, [FromUri] string taskStatus)
         {
             _taskMonitoringService.UpdateTaskResponseStatus(taskName, taskStatus);
+        }
+    }
+
+
+    public class LoadBalancerStatusController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                using (
+                    var file = new FileStream(@"/etc/lbstatus/taskscheduler", FileMode.Open,
+                        FileAccess.Read, FileShare.ReadWrite))
+                using (var reader = new StreamReader(file))
+                {
+                    return new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(reader.ReadToEnd())
+                    };
+                }
+            }
+            catch (IOException)
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("OTWEB_OFF")
+                };
+            }
         }
     }
 }
