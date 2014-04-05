ba0e7cb481156fbbe3a7078a1608a3a7e967132a
orlando <operri@opentable.com>
2014-04-05 17:37:33 +0100

    Removed the web api controller

--- Frontend/Controllers/ResponseController.cs
+++ Frontend/Controllers/ResponseController.cs
@@ -1,28 +1,13 @@
 ﻿using System.Configuration;
 using System.IO;
 using System.Web.Http;
+using System.Web.Mvc;
 using MongoDataAccess;
 using TaskScheduler.Services;
 
 namespace Frontend.Controllers
 {
-    public class ResponseController : ApiController
-    {
-        private readonly ITaskMonitoringService _taskMonitoringService;
-
-        public ResponseController()
-        {
-            _taskMonitoringService = new TaskMonitoringService(new MongoTaskRepository(ConfigurationManager.AppSettings["MongoUrl"]));
-        }
-
-        public void Post([FromUri] string taskName, [FromUri] string taskStatus)
-        {
-            _taskMonitoringService.UpdateTaskResponseStatus(taskName, taskStatus);
-        }
-    }
-
-
-    public class LoadBalancerStatusController : ApiController
+    public class LoadBalancerStatusController : Controller
     {
         public string Get()
         {
