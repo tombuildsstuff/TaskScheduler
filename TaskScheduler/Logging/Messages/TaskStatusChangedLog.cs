2be73673264592b56bba6b5a1a78b9fde5a36a04
tombuildsstuff <tharvey@opentable.com>
2014-12-22 10:45:41 +0000

    Added a new log message

---
+++ TaskScheduler/Logging/Messages/TaskStatusChangedLog.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskScheduler.Logging.Messages
+{
+    public class TaskStatusChangedLog : LogStashLog
+    {
+        public TaskStatusChangedLog(string taskName, ResponseStatus status)
+        {
+            TaskName = taskName;
+            Status = status;
+        }
+
+        public ResponseStatus Status { get; set; }
+
+        public string TaskName { get; set; }
+
+        protected override string Type
+        {
+            get { return "TaskStatusChangedLog"; }
+        }
+    }
+}
